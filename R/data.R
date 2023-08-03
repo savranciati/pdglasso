ea7513fa49da4f6b0da1cae437cdebc0d71f79fe
Saverio Ranciati <sav.ranciati@gmail.com>
2023-08-03 15:13:18 +0200

    Regenerated toy_data.

--- R/data.R
+++ R/data.R
@@ -4,13 +4,13 @@
 #' * `p=20`
 #' * `type=c("v", "i","a")`
 #' * `force.symm=NULL`
-#' * `dens=0.3`
+#' * `dens=0.5`
+#' * `Sigma` a \eqn{p \times p} matrix with unitary diagonal and 0.5 on off-diagonal elements.
 #'
 #' @format ## `toy_data`
 #' A list with the following components:
 #'
 #' * `pdColG` a matrix representing a coloured graph for paired data; see [`pdglasso-package`] for details.
-#'
 #' * `K`, a concentration matrix adapted to `pdColG`.
 #' * `sample.data`, a data frame with 60 rows and 20 columns from a multivariate normal distribution
 #' with zero mean vector and concentration matrix `K`.
